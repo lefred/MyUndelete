3894aea9ff5aaa375c9e2eb011a942d48bc0a7c5
frederic.descamps <frederic.descamps@percona.com>
2014-11-20 00:48:47 +0100

    still in progress (not working yet)
    Signed-off-by: frederic.descamps <frederic.descamps@percona.com>

--- MyUndelete.py
+++ MyUndelete.py
@@ -14,6 +14,7 @@
 import base64
 import sys, getopt
 import subprocess
+import re
 from distutils.util import strtobool
 
 def main(argv):
@@ -22,18 +23,19 @@ def main(argv):
    endpos = ''
    check_insert = False
    try:
-      opts, args = getopt.getopt(argv,"hb:e:is:",["binlog=","end=","insert","start="])
+      opts, args = getopt.getopt(argv,"hb:e:is:u",["binlog=","end=","insert","start=","update"])
    except getopt.GetoptError:
-      print 'MyUndelete.py -b <binlog> -s <start position> -e <end position> [-i]'
+      print 'MyUndelete.py -b <binlog> -s <start position> -e <end position> [-i] [-u]'
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print 'MyUndelete.py -b <binlog> -s <start position> -e <end position> [-i]'
+         print 'MyUndelete.py -b <binlog> -s <start position> -e <end position> [-i] [-u]'
          print ''
          print '  -b | --binlog=  : path of the binary log file'
          print '  -s | --start=   : start position'
          print '  -e | --end=     : stop position'
          print '  -i | --insert   : consider also INSERT statements (by default, only DELETE)'
+         print '  -u | --update   : consider also UPDATE statements (by default, only DELETE)'
          print ''
          print 'Info: The program expects that you have read access to the binary log'
          print 'and you have all eventual MySQL credential in ~/.my.cnf'
@@ -47,6 +49,8 @@ def main(argv):
          endpos = arg
       elif opt in ("-i", "--insert"):
          check_insert = True
+      elif opt in ("-u", "--update"):
+         check_update = True
 
    if binlog == '':
        print "ERROR: binlog file is required !"
@@ -60,7 +64,7 @@ def main(argv):
    print 'Binlog file is ', binlog
    print 'Start Position file is ', startpos
    print 'End Postision file is ', endpos 
-   return(binlog, startpos, endpos, check_insert)
+   return(binlog, startpos, endpos, check_insert, check_update)
 
 def user_yes_no_query(question):
     sys.stdout.write('%s [y/n]\n' % question)
@@ -71,7 +75,7 @@ def user_yes_no_query(question):
             sys.stdout.write('Please respond with \'y\' or \'n\'.\n')
 
 
-def mysqlbinlog(binlog, startpos, endpos, check_insert):
+def mysqlbinlog(binlog, startpos, endpos, check_insert, check_update):
 
   if check_insert:
       print "We also look to undo INSERTs"
@@ -79,10 +83,14 @@ def mysqlbinlog(binlog, startpos, endpos, check_insert):
   c1 = ['/usr/bin/sudo', '/usr/bin/mysqlbinlog', '--start-position=%s' % startpos, '--stop-position=%s' % endpos, binlog]
   p1 = subprocess.Popen(c1, stdout=subprocess.PIPE)
  
-  c2 = ['awk', 'c&&!--c;/^BINLOG /{c=2}']
+  #c2 = ['awk', 'c&&!--c;/^BINLOG /{c=2}']
+  c2 = ['awk', '/\/*!*\/\;/{flag=0}flag;/^BINLOG /{flag=1}']
   p2 = subprocess.Popen(c2, stdin=p1.stdout, stdout=subprocess.PIPE)
  
   found_del = False 
+  found_update = False 
+  get_full_binlog = False 
+  binlog_event = []
   for line in iter(p2.stdout.readline, b''):
       base64line = line.rstrip()
       try:
@@ -90,9 +98,15 @@ def mysqlbinlog(binlog, startpos, endpos, check_insert):
       except:
         print "ERROR: no valid event found !"
         sys.exit(4) 
+      if found_update:
+        binlog_event.append(base64line)
+        continue  
       old_header = decodedline[:10]
       new_header = list(old_header)
-      event_type = old_header[4]
+      try:
+        event_type = old_header[4]
+      except:
+         event_type = '' 
       if event_type == '\x19':
          found_del = True
          print "ROW event : %s" % base64line
@@ -120,6 +134,11 @@ def mysqlbinlog(binlog, startpos, endpos, check_insert):
          new_header[4] = ' '
          new_encodedheader = base64.b64encode(''.join(new_header))[:-2]
          old_encodedheader = base64.b64encode(old_header)[:-2]
+      elif event_type == '\x1f' and check_update:
+         found_update = True
+         print "ROW event : %s" % base64line
+         print "Event type (%s) is an update v2" % repr(event_type)
+         binlog_event.append(base64line)
 
       if found_del:
          print "Old header = %s" % old_encodedheader
@@ -138,8 +157,100 @@ def mysqlbinlog(binlog, startpos, endpos, check_insert):
             sys.exit(0) 
          else:
             print "Bye...bye... my data"
-  if not found_del:
-         print "Nothing to do..."
+  if found_update:
+      print "We got an update!!"
+      for i in binlog_event[:-1]:
+          print "Binlog line : %s" % i
+      for i in binlog_event[:-1]:
+          print "Binlog line decoded : %s" % repr(base64.b64decode(i))
+      # let's  consider that the PK is always on one binlog line
+      # check that we have currently the right binlog line 
+      if base64.b64decode(binlog_event[0])[31] != "\xff":
+          print "ERROR: problem parsing binary log header"
+          sys.exit(5)
+      # find the "marker" and the PK
+      to_find = base64.b64decode(binlog_event[0])[32] + base64.b64decode(binlog_event[0])[33]
+      got_it_in = -1
+      old_image = [None]*(len(binlog_event)-1)
+      new_image = [None]*(len(binlog_event)-1)
+      old_queue = [None]*(len(binlog_event)-1)
+      got_position = False
+      for binlog_line in binlog_event[:-1]:
+          got_it_in += 1
+          position = base64.b64decode(binlog_line).rfind(to_find)
+          print "Position = %d in line %d" % (position, got_it_in)
+          if got_it_in == (len(binlog_event)-2):
+              # we need to keep the queue
+              old_queue[got_it_in]  = base64.b64decode(binlog_line)[-2:]
+          if not position and not got_position:
+              old_image[got_it_in] = base64.b64decode(binlog_line)
+              continue
+          elif position == 32 and got_it_in == 0:
+             # we need to find it on another line
+             old_header = base64.b64decode(binlog_line)[:32]
+             old_image[got_it_in] = base64.b64decode(binlog_line)[32:]
+             continue
+          else:              
+            if position:
+                got_position = True
+            if got_it_in == 0:
+                # now we need to retrieve the value from 32 to the position
+                # as the new value is also on the first line
+                old_image[got_it_in]=base64.b64decode(binlog_line)[32:position]
+            else:
+                old_image[got_it_in]=base64.b64decode(binlog_line)[:position]
+
+            if got_it_in == (len(binlog_event)-2) and position:
+              new_image[got_it_in]=base64.b64decode(binlog_line)[position:-2]
+            elif got_it_in == (len(binlog_event)-2):
+              new_image[got_it_in]=base64.b64decode(binlog_line)[:-2]
+            elif position:
+              new_image[got_it_in]=base64.b64decode(binlog_line)[position:]
+            else:
+              new_image[got_it_in]=base64.b64decode(binlog_line)
+
+      replacement = []
+      original = []
+      # we need to rebuild the binary log event
+      for i in range(len(binlog_event)-1):
+          print "   EVENT (%s) : %s" % (i, base64.b64encode(binlog_event[i]))
+          print "   old_header : " + base64.b64encode(old_header)
+          print "   old_image  : " + base64.b64encode(old_image[i])
+          print "   new_image : " + base64.b64encode(new_image[i])
+          print "   old_queue : " + base64.b64encode(old_queue[i])
+          str = ""
+          if i == 0:
+             str = old_header + new_image[i]
+          if new_image[i]: 
+             str = str + new_image[i]
+          if old_queue[i]:
+             str = str + old_queue[i]
+          replacement.append(base64.b64encode(str))
+          original.append(binlog_event[i])
+          #replacement.append(re.escape(base64.b64encode(str)).replace('\\+','+'))
+          #original.append(re.escape(binlog_event[i]).replace('\\+','+'))
+
+      print "OLD : " 
+      print  original
+      print "NEW : " 
+      print replacement
+      if user_yes_no_query("Ready to revert the statement ?"):
+          c1 = ['/usr/bin/sudo', '/usr/bin/mysqlbinlog', '--start-position=%s' % startpos, '--stop-position=%s' % endpos, binlog]
+          p1 = subprocess.Popen(c1, stdout=subprocess.PIPE)
+ 
+          c2 = ['sed', "s/^%s/%s/" % (original, replacement)]
+          p2 = subprocess.Popen(c2, stdin=p1.stdout, stdout=subprocess.PIPE)
+         
+          c3 = ['mysql']
+          p3 = subprocess.Popen(c3, stdin=p2.stdout, stdout=subprocess.PIPE)
+         
+          print "Done... I hope it worked ;)"
+          sys.exit(0) 
+      else:
+          print "Bye...bye... my data"
+
+  elif not found_del:
+      print "Nothing to do..."
         
         
 
@@ -147,6 +258,6 @@ if __name__ == "__main__":
    print ""
    print "*** WARNING *** USE WITH CARE ****"
    print ""
-   (binlog, startpos, endpos, check_insert)=main(sys.argv[1:])
-   mysqlbinlog(binlog, startpos, endpos, check_insert)
+   (binlog, startpos, endpos, check_insert, check_update)=main(sys.argv[1:])
+   mysqlbinlog(binlog, startpos, endpos, check_insert, check_update)
 
