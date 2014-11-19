889faa9668c698db6fdfb00dc95b3c1d75d4eea1
frederic.descamps <frederic.descamps@percona.com>
2014-11-19 07:51:55 +0100

    Handle reverting insert events
    Signed-off-by: frederic.descamps <frederic.descamps@percona.com>

--- MyUndelete.py
+++ MyUndelete.py
@@ -20,14 +20,15 @@ def main(argv):
    binlog = ''
    startpos = ''
    endpos = ''
+   check_insert = False
    try:
-      opts, args = getopt.getopt(argv,"hb:e:s:",["binlog=","start=","end="])
+      opts, args = getopt.getopt(argv,"hb:e:is:",["binlog=","end=","insert","start="])
    except getopt.GetoptError:
-      print 'MyUndelete.py -b <binlog> -s <start position> -e <end position>'
+      print 'MyUndelete.py -b <binlog> -s <start position> -e <end position> [-i]'
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print 'MyUndelete.py -b <binlog> -s <start position> -e <end position>'
+         print 'MyUndelete.py -b <binlog> -s <start position> -e <end position> [-i]'
          print ''
          print 'The program expects that you have read access to the binary log'
          print 'and you have all eventual MySQL credential in ~/.my.cnf'
@@ -38,6 +39,8 @@ def main(argv):
          startpos = arg
       elif opt in ("-e", "--end"):
          endpos = arg
+      elif opt in ("-i", "--insert"):
+         check_insert = True
 
    if binlog == '':
        print "ERROR: binlog file is required !"
@@ -51,7 +54,7 @@ def main(argv):
    print 'Binlog file is ', binlog
    print 'Start Position file is ', startpos
    print 'End Postision file is ', endpos 
-   return(binlog, startpos, endpos)
+   return(binlog, startpos, endpos, check_insert)
 
 def user_yes_no_query(question):
     sys.stdout.write('%s [y/n]\n' % question)
@@ -62,7 +65,10 @@ def user_yes_no_query(question):
             sys.stdout.write('Please respond with \'y\' or \'n\'.\n')
 
 
-def mysqlbinlog(binlog, startpos, endpos):
+def mysqlbinlog(binlog, startpos, endpos, check_insert):
+
+  if check_insert:
+      print "We also look to undo INSERTs"
   #import pdb; pdb.set_trace()
   c1 = ['/usr/bin/sudo', '/usr/bin/mysqlbinlog', '--start-position=%s' % startpos, '--stop-position=%s' % endpos, binlog]
   p1 = subprocess.Popen(c1, stdout=subprocess.PIPE)
@@ -77,6 +83,7 @@ def mysqlbinlog(binlog, startpos, endpos):
       old_header = decodedline[:10]
       new_header = list(old_header)
       event_type = old_header[4]
+      print repr(event_type)
       if event_type == '\x19':
          found_del = True
          print "ROW event : %s" % base64line
@@ -90,12 +97,25 @@ def mysqlbinlog(binlog, startpos, endpos):
          print "Event type (%s) is a delete v2" % repr(event_type)
          new_header[4] = '\x1e'
          new_encodedheader = base64.b64encode(''.join(new_header))[:-2]
+      elif event_type == '\x17':
+         found_del = True
+         print "ROW event : %s" % base64line
+         print "Event type (%s) is an insert v1" % repr(event_type)
+         new_header[4] = '\x19'
+         new_encodedheader = base64.b64encode(''.join(new_header[:8]))[:-2]
+         old_encodedheader = base64.b64encode(old_header[:8])[:-2]
+      elif event_type == '\x1e' and check_insert:
+         found_del = True
+         print "ROW event : %s" % base64line
+         print "Event type (%s) is an insert v2" % repr(event_type)
+         new_header[4] = ' '
+         new_encodedheader = base64.b64encode(''.join(new_header))[:-2]
          old_encodedheader = base64.b64encode(old_header)[:-2]
 
       if found_del:
          print "Old header = %s" % old_encodedheader
          print "New header = %s" % new_encodedheader
-         if user_yes_no_query("Ready to re-insert the deleted record(s) ?"):
+         if user_yes_no_query("Ready to revert the statement ?"):
             c1 = ['/usr/bin/sudo', '/usr/bin/mysqlbinlog', '--start-position=%s' % startpos, '--stop-position=%s' % endpos, binlog]
             p1 = subprocess.Popen(c1, stdout=subprocess.PIPE)
  
@@ -116,6 +136,6 @@ if __name__ == "__main__":
    print ""
    print "*** WARNING *** USE WITH CARE ****"
    print ""
-   (binlog, startpos, endpos)=main(sys.argv[1:])
-   mysqlbinlog(binlog, startpos, endpos)
+   (binlog, startpos, endpos, check_insert)=main(sys.argv[1:])
+   mysqlbinlog(binlog, startpos, endpos, check_insert)
 
